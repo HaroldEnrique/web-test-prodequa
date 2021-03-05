449b2a767b534977d492916c1ef0cc25d6ac50f6
HaroldEnrique <haroldcotacallapa@upeu.edu.pe>
2021-03-04 23:51:01 -0500

    WEB v1 challenge done

---
+++ README.md
@@ -0,0 +1,14 @@
+## WEB-TEST-PRODEQUA
+
+- VueJS 3 (CDN)
+- TailwindCSS 2
+
+## INSTALL
+Install dependencies declared at package.json file
+- npm install
+
+Compile Tailwindcss styles
+- npm run build
+
+Run server
+- live-server public
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#6200EE',
+        'primary-variant': '#3700B3',
+        'secondary': '#03DAC6',
+        'secondary-variant': '#018786',
+        'teal': {
+          '50':  '#F0FDFA',
+          '100': '#CCFBF1',
+          '200': '#99F6E4',
+          '300': '#5EEAD4',
+          '400': '#2DD4BF',
+          '500': '#14B8A6',
+          '600': '#0D9488',
+          '700': '#0F766E',
+          '800': '#115E59',
+          '900': '#134E4A',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
